b4799f48deb61ef22c0d9f949438d7ca0328388a
Jonathan Szkup <jszkup101@gmail.com>
2023-10-19 12:23:59 -0400

    Create 256 divs with a loop, turn into 16x16 grid

--- scripts.js
+++ scripts.js
@@ -1,4 +1,15 @@
 // 16 * 16 = 256
 
-// function createGrid()
+function createGrid() {
+    const container = document.getElementById("gridContainer");
 
+    for (let i = 0; i < 256; ++i) {
+        let div = document.createElement("div");
+
+        div.textContent = ` TEST ${i + 1} `;
+        container.appendChild(div)
+    }
+}
+
+
+createGrid()
